364770866141659140f07519b304eb2f67bdce79
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-04-29 15:03:58 +0900

    Title: 변수 값 복사하기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250429/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 5;
+    int c = 3;
+
+    c = a;
+    a = a + c;
+    b = b - c;
+
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    return 0;
+}
